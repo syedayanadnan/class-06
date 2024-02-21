ce5302f1c615393312fb6a5dcc98898022792817
syedayanadnan <youremail@example.com>
2024-02-21 13:25:10 +0500

    conversion of datatypes

--- 03_conversionOpration.js
+++ 03_conversionOpration.js
@@ -1,14 +1,14 @@
 let scoure = "syed"
-// console.log(typeof scoure);
+console.log(typeof scoure);
 let valueInNumber = Number(scoure)
-// console.log(typeof valueInNumber );
-// console.log(valueInNumber);
+console.log(typeof valueInNumber );
+console.log(valueInNumber);
 // "22" => 22
 //"22abc" => NaN
 //ture => 1; false => 0
 let isLoggedIn = 0
-// let BooleanIsLoggedIn = Boolean (isLoggedIn)
-// console.log(BooleanIsLoggedIn);
+let BooleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(BooleanIsLoggedIn);
 // 1 => true
 // "syed ayan" =>true 
 // 0 => fales
