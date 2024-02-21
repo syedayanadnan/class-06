f9f1a3d47d5d44a2ca5b8f71f24c0d9f358bbd9e
syedayanadnan <youremail@example.com>
2024-02-21 13:21:57 +0500

    first commit

---
+++ 03_conversionOpration.js
@@ -0,0 +1,38 @@
+let scoure = "syed"
+// console.log(typeof scoure);
+let valueInNumber = Number(scoure)
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber);
+// "22" => 22
+//"22abc" => NaN
+//ture => 1; false => 0
+let isLoggedIn = 0
+// let BooleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+// 1 => true
+// "syed ayan" =>true 
+// 0 => fales
+let someNumber = 22
+let stringNumber =  String (someNumber)
+console.log( stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
